3bca6a56e95d653b6f006c3ddb3d2f946b7654de
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2023-01-15 17:29:26 +0100

    Jersyfi linking in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,13 +11,15 @@ const Footer = () => {
                 &copy;{getCurrentYear()} Jersyfi
             </p>
 
-            <Image
-                className="h-8 mx-auto"
-                src="/img/JERSYFI.svg"
-                alt="Jersyfi Schriftzug"
-                height={32}
-                width={128}
-            />
+            <a href="https://jersyfi.de">
+                <Image
+                    className="h-8 mx-auto"
+                    src="/img/JERSYFI.svg"
+                    alt="Jersyfi Schriftzug"
+                    height={32}
+                    width={128}
+                />
+            </a>
         </footer>
     )
 }
