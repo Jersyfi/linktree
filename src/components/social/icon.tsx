35016899e6c1021fcddab5f66a3ad508c29b4af0
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2023-04-02 17:16:45 +0200

    Twitter Icon

--- src/components/social/icon.tsx
+++ src/components/social/icon.tsx
@@ -9,6 +9,7 @@ const Icon: React.FC<Props> = ({ icon }) => {
     const iconClasses = {
         steam: "p-2 bg-gradient-steam rounded-full shadow-md hover:bg-steam-500 focus:outline-none focus:ring-2 focus:ring-steam-400 focus:ring-opacity-75",
         instagram: "p-2 bg-gradient-instagram rounded-full shadow-md hover:bg-instagram-400 focus:outline-none focus:ring-2 focus:ring-instagram-300 focus:ring-opacity-75",
+        twitter: "p-2 bg-twitter-600 rounded-full shadow-md hover:bg-twitter-500 focus:outline-none focus:ring-2 focus:ring-twitter-600 focus:ring-opacity-75",
         spotify: "p-2 bg-spotify-500 rounded-full shadow-md hover:bg-spotify-300 focus:outline-none focus:ring-2 focus:ring-spotify-400 focus:ring-opacity-75",
         snapchat: "p-2 bg-snapchat-500 rounded-full shadow-md hover:bg-snapchat-200 focus:outline-none focus:ring-2 focus:ring-snapchat-300 focus:ring-opacity-75",
         youtube: "p-2 bg-youtube-600 rounded-full shadow-md hover:bg-youtube-400 focus:outline-none focus:ring-2 focus:ring-youtube-500 focus:ring-opacity-75",
